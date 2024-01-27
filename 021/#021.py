c17b75d48abc823554b1b6fd45252183382f5e7d
Leomaran Senthan <156711626+LeoSenthan@users.noreply.github.com>
2024-01-27 20:34:22 +0000

    Update #021.py

--- 021/#021.py
+++ 021/#021.py
@@ -1,8 +1,14 @@
 pairs=[0,0]
-for n in range(2,10):
+for n in range(2,10000):
     total=0
-    for factor in range(1,n):
+    for factor in range(1,n//2+1):
         if n%factor==0:
             total+=factor
-    pairs.append(factor)
-print(pairs)
+    pairs.append(total)
+total=0
+for n in range(0,len(pairs)):
+    if pairs[n]<len(pairs)-1:
+        if n==pairs[pairs[n]] and n!=pairs[n]:
+            total=total+n+pairs[pairs[n]]
+            print(pairs[n],n)
+print(int(total/2))
