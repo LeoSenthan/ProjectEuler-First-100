195a8ca6e8a95cbf12434f3c838f3209ba1b5da6
Leomaran Senthan <156711626+LeoSenthan@users.noreply.github.com>
2024-01-27 20:04:25 +0000

    update

--- 018/#018.py
+++ 018/#018.py
@@ -1,3 +1,8 @@
 with open("018/#018.txt","r") as f:
     triangle=f.readlines()
-print(triangle)
+for row in range(0,len(triangle)):
+    triangle[row]=triangle[row].replace("\n","").split(" ")
+    print(triangle[row])
+for layer in range(len(triangle),0,-1):
+    for number in range(0,len(triangle[layer])):
+        pass
