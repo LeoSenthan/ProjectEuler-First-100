9dc275222601a5fdb2a8435aa727b1a4d1dd5305
Leomaran Senthan <156711626+LeoSenthan@users.noreply.github.com>
2024-02-23 18:23:19 +0000

    rewrite with shorter variable names to increase speed

--- 054/#054.py
+++ 054/#054.py
@@ -1,38 +1,46 @@
 from itertools import *
-SCORE = []
 
-def score_func(func):
-    SCORE.append(func)
-    return func
-def groups(cards, length):
-    return [k for k, g in groupby(sorted(value for value, suit in cards)) if len(list(g)) == length]
-def pairwise(cards):
-    a, b = tee(sorted(value for value, suit in cards))
+S = []
+
+def s_func(f):
+    S.append(f)
+    return f
+
+def g(cards, length):
+    return [k for k, g in groupby(sorted(v for v, s in cards)) if len(list(g)) == length]
+
+def p(cards):
+    a, b = tee(sorted(v for v, s in cards))
     next(b, None)
     return zip(a, b)
+
 def suits(cards):
-    return list({suit for value, suit in cards})
-def cards(hand):
-    faces = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
-    return sorted(((int(faces.get(card[0], card[0])), card[1]) for card in hand), key=lambda x: x[0])
+    return list({s for v, s in cards})
+
+def c(hand):
+    f = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    return sorted(((int(f.get(c[0], c[0])), c[1]) for c in hand), key=lambda x: x[0])
+
 def parse_hands(lines):
-    return ((cards(line.split()[:5]), cards(line.split()[5:])) for line in lines)
+    return ((c(l.split()[:5]), c(l.split()[5:])) for l in lines)
+
 def winner(hands):
     return int(score(hands[0]) < score(hands[1])) + 1
+
 def score(cards):
-    highest_card = max(value for value, suit in cards)
-    pairs = groups(cards, 2)
+    highest_card = max(v for v, s in cards)
+    pairs = g(cards, 2)
     one_pair = 1e2 + 2e1 * max(pairs) if len(pairs) == 1 else 0
     two_pair = 1e3 + 2e2 * max(pairs) if len(pairs) == 2 else 0
-    threes = groups(cards, 3)
+    threes = g(cards, 3)
     three_of_a_kind = 1e4 + 2e3 * max(threes) if len(threes) == 1 else 0
-    is_straight = 1e5 * int(all(b - a == 1 for a, b in pairwise(cards)))
+    is_straight = 1e5 * int(all(b - a == 1 for a, b in p(cards)))
     is_flush = 1e6 * int(len(suits(cards)) == 1)
-    full_house = 1e7 * int(len(groups(cards, 2)) == 1 and len(groups(cards, 3)) == 1)
-    fours = groups(cards, 4)
+    full_house = 1e7 * int(len(g(cards, 2)) == 1 and len(g(cards, 3)) == 1)
+    fours = g(cards, 4)
     four_of_a_kind = 1e8 + 2e7 * max(fours) if len(fours) == 1 else 0
-    is_straight_flush = 1e9 * int(len(suits(cards)) == 1 and all(b - a == 1 for a, b in pairwise(cards)))
-    is_royal_flush = 1e10 * int(len(suits(cards)) == 1 and [value for value, suit in cards] == [10, 11, 12, 13, 14])
+    is_straight_flush = 1e9 * int(len(suits(cards)) == 1 and all(b - a == 1 for a, b in p(cards)))
+    is_royal_flush = 1e10 * int(len(suits(cards)) == 1 and [v for v, s in cards] == [10, 11, 12, 13, 14])
     return sum([highest_card, one_pair, two_pair, three_of_a_kind, is_straight, is_flush, full_house, four_of_a_kind, is_straight_flush, is_royal_flush])
 
 with open('054/#054.txt', "r") as f:
