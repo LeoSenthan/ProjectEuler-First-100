0ac0bed16a82e757c4e7b1b77cef37af12b10ded
Leomaran Senthan <156711626+LeoSenthan@users.noreply.github.com>
2024-03-16 16:56:36 +0000

    2 more done now at 144 total i think

--- BONUS/243/#243.py
+++ BONUS/243/#243.py
@@ -1,25 +1,15 @@
-#relative prime
-import math
-def primeFactors(n):
-  factors=[]
-  while n % 2 == 0:
-      factors.append(2)
-      n = n // 2
-  for i in range(3,int(math.sqrt(n))+1,2):
-      while n % i== 0:
-          factors.append(i)
-          n = n // i
-  if n > 2:
-      factors.append(n)
-  return factors,set(factors)
-ratio,denominator=1,11
-while ratio>4/10:
-    denominator+=1
-    count=denominator
-    factors,uniquefactors=list(primeFactors(denominator))
-    for unique in list(set(factors)):
-        count/=unique
-        count*=(factors.count(unique)-1)
-    print(count,denominator)
-    ratio=count/denominator
-print(denominator)
+#answer is a multiple of primes 2*3*5*7*11*13*17*19*23
+#first and last are always resilient
+count=0
+for n in range(1,(8*3*5*7*11*13*17*19*23),2):
+        if n%3!=0:
+            if n%5!=0:
+                if n%7!=0:
+                    if n%11!=0:
+                         if n%13!=0:
+                              if n%17!=0:
+                                   if n%23!=0:
+                                        count+=1
+print(count/(8*3*5*7*11*13*17*19*23))
+print(8*3*5*7*11*13*17*19*23)
+#tried multiples of this got lucky
--- Second_Batch_Of_100/132/#132.py
+++ Second_Batch_Of_100/132/#132.py
@@ -1,35 +1,22 @@
-def repunit_sum(k):
-    return (10**k - 1) // 9
-# one of the factors is 11 as the difference between odd digits and even digits are 0
-
-import math
-def primeFactors(n):
-  factors=[]
-  while n % 2 == 0:
-      factors.append(2)
-      n = n // 2
-  for i in range(3,int(math.sqrt(n))+1,2):
-      while n % i== 0:
-          factors.append(i)
-          n = n // i
-  if n > 2:
-      factors.append(n)
-  return factors
-
-factors=primeFactors(repunit_sum(13))
-print(factors)
-#r(3)=3, 37
-#R(4)= 11,101
-#R(5) = 41 *271
-#R(6) = 3 * 7 * 11 * 13 *37
-#R(7) = 239*4649
-#R(8) = 11 73 101 137
-#R(9) = 3 3 37 333667
-#R(10) = 11 41 271 9091
-#r(11) = 21649 513239
-#r(12) = 3,7,11,13,101,9901
-#r(13) = 53,79,265371653
-# First factor is not 2,3,5 could be 7
-#Has to be 11 as on even k lengths 11 is a power
-#After dividing by 11 the number now becomes 101010... with the same length -1 ends in a 1
-#Factorized = 11*int(str(1)+"01"*10**9/2-1)
+#R(k)= (10*k-1)/9 remainder of 10*k and 9*n is 1 if r(k) can be divided by n
+def SieveOfEratosthenes(num):
+  list1=[]
+  prime = [True for i in range(num+1)]
+  p = 2
+  while (p * p <= num):
+      if (prime[p] == True):
+          for i in range(p * p, num+1, p):
+              prime[i] = False
+      p += 1
+  for p in range(2, num+1):
+      if prime[p]:
+          list1.append(p)
+  return list1
+primes=SieveOfEratosthenes(1000001)
+sum,n=0,0
+for p in primes:
+    if pow(10, 10**9, 9*p)==1:
+        n += 1
+        sum += p
+        if n==40: break
+print (sum)
