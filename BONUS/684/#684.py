a2cde9a1cf3b5c1446cd77b79ceeeeb19b74f9fa
Leomaran Senthan <156711626+LeoSenthan@users.noreply.github.com>
2025-11-07 17:52:21 +0000

    sdas

--- BONUS/684/#684.py
+++ BONUS/684/#684.py
@@ -1,34 +1,16 @@
-def power_modulo(base, exponent, modulus):
-    result = 1
-    while exponent > 0:
-        if exponent % 2 == 1:
-            result = (result * base) % modulus
-        base = (base * base) % modulus
-        exponent //= 2
-    return result
+import sys
+sys.set_int_max_str_digits(1000000000)
 
-def remainder_large_number_single_digit(base, exponent, modulus):
-    # Compute the remainder of the base
-    base_remainder = base % modulus
-    # Compute the remainder of 10^exponent
-    exponent_remainder = power_modulo(10, exponent, modulus)
-    # Compute the final remainder
-    remainder = (base_remainder * exponent_remainder) % modulus
-    return remainder
+def s(n):
+    return int(str(n%9)+"9" *(n//9))
 
-# Initialize total
-total = 0
-fibbonaci=[1,1]
-while len(fibbonaci)<90:
-    fibbonaci.append(fibbonaci[-1]+fibbonaci[-2])
-# Iterate over the range
-for i in fibbonaci[1:]:
-    if i // 9 < 1:
-        total += i
-    else:
-        # Calculate the remainder using modular arithmetic
-        remainder = remainder_large_number_single_digit(i % 9 + 1, i // 9, 1000000007)
-        # Add to total
-        total += remainder-1
+def S(k):
+    return sum([s(n) for n in range(1,k+1)])
 
-print(total %1000000007)
+curr = 1
+prev = 1
+count = 2
+total = 0 
+while count <10:
+    print(S(curr)-S(prev))
+    curr,prev  = curr + prev, curr
